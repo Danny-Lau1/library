8eced3aaf83a958b8e0cbe980aa59f486a8f9e77
Danny <dannylau@Dannys-MacBook-Air.local>
2024-02-13 15:54:23 -0800

    add modal and form to submit new book

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let addBookBtn = document.getElementById("showModal");
+let modal = document.getElementById("modal");
+
+
+let form = document.getElementById("form")
+let submitBtn = document.getElementById("submit")
+
+addBookBtn.addEventListener("click", () => {
+    modal.showModal()
+})
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    // Book Information From Form Inputs
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value
+    let pages = document.getElementById("pages").value
+    let readStatus = document.getElementById("read").value
+    console.log(title)
+
+    let newBook = new BookConstructor(title, author, pages, readStatus)
+    addBook(newBook)
+})
+
+
+
 let myLibrary = [];
 
 function BookConstructor(title, author, pages, beenRead) {
@@ -7,10 +34,14 @@ function BookConstructor(title, author, pages, beenRead) {
     this.beenRead = beenRead;
 }
 
-book1 = new BookConstructor("Harry Potter", "J.K Rowling", "300", true)
-
-book2 = new BookConstructor("Ready Player One", "Ernest Cline", "350", False)
+// 
 
 function addBook(book) {
-    myLibrary.append(book)
+    myLibrary.push(book)
+    console.log(myLibrary[0])
 }
+
+//book1 = new BookConstructor("Harry Potter", "J.K Rowling", "300", true)
+
+// book2 = new BookConstructor("Ready Player One", "Ernest Cline", "350", False)
+
